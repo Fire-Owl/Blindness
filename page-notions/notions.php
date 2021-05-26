05b003b685cd959e3fd2c1bdf614bb655c736e3c
williams <w.chazal@codeur.online>
2021-05-26 16:12:09 +0200

    début intégration

---
+++ page-notions/notions.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="notions.css">
+    <title>Notions</title>
+</head>
+<body>
+
+<div class="main">
+        <div class="main__title"><h1>I. Notions</h1></div>
+        <div class="main__article--top">
+            <div class="main__article--top-title"><h2>Handicap</h2></div>
+            <div class="main__article--top-left"><p>Le handicap est une notion complexe et évolutive. Il désigne l'incapacité d'un individu à vivre et agir pleinement dans son environnement, en raison de déficiences physiques, mentales ou sensorielles.<br>
+            À partir de 1980, le terme prend une dimension sociale. On prend conscience que ce qui créer la situation de handicap résulte d'avantage d'un environnement inadapté que de la déficience elle-même.</p></div>
+             <div class="main__article--top-right"><p>Le handicap souffre d’une image erronée, se résumant principalement à des personnes en fauteuils roulants. Détrompez-vous, 80 à 85% des situations de handicap sont de nature imperceptible. En effet, comment pourriez-vous devinez qu'untel souffre de diabète et qu'un autre souffre de dyslexie ?</p></div>
+        </div>
+        <div class="main__article--bottom">
+            <div class="main__article--bottom-title"><h2>Accessibilité</h2></div>
+            <div class="main__article--bottom-left"><p>Elle peut se traduire par l’accès à tous pour tous. Dans notre cadre, elle consiste à mettre les personnes en situation de handicap au même pied d'égalité que les autres. Pour cela on essaie de réduire au minimum les inégalités liées à l'environnement. </p></div>
+        </div>
+</div>
+</body>
+</html>
