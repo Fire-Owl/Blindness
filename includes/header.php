8caad53c7f4842c5141e77ad53f613da898828b2
Gaël Sudour-Jeannin <67095523+Sastaras@users.noreply.github.com>
2021-05-27 16:39:23 +0200

    Header First Commit

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Blindness</title>
+    <link rel="stylesheet" type="text/css" href="styles/header.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="nav">
+        <a href="index.php"><img src="assets/logo/blindness.svg" alt="Logo du site Blindness" class="logo">
+            <h1 class="logo-text noselect">Blindness</h1>
+        </a>
+        <label for="toggle" class="noselect">Navigation</label>
+        <input type="checkbox" id="toggle" />
+        <div class="menu">
+            <div class="nav-menu-romanum">
+                <a href="#">I.</a>
+                <a href="#">II.</a>
+                <a href="#">III.</a>
+                <a href="#">IV.</a>
+            </div>
+            <div class="nav-menu-name">
+                <a href="#">NOTIONS</a>
+                <a href="#">DEFICIENCES VISUELLES</a>
+                <a href="#">OUTILS</a>
+                <a href="#">BONNES PRATIQUES</a>
+            </div>
+        </div>
+    </div>
