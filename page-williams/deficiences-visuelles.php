a0ce58c2257a36378b8fcfef8d414967ae30bd7c
williams <w.chazal@codeur.online>
2021-05-27 16:48:26 +0200

    page-notions Ok, début page déficiences, Responsive Ok

---
+++ page-williams/deficiences-visuelles.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="notions.css">
+    <title>deficiences-visuelles</title>
+</head>
+<body>
+
+<div class="main">
+        <div class="main__title"><h1>II. Déficiences visuelles</h1></div>
+
+        <div class="main__article--top">
+            <div class="main__article--title"><h2>Définition</h2></div>
+            <div class="main__article--top-left"><p>La déficience visuelle désigne les troubles liés à la fonction visuelle. Elle repose sur deux critères : l'état du champ visuel et l’acuité visuelle. À partir de là, on distingue deux catégories de personnes atteintes de déficience. Les aveugles dont l’acuité est égale ou inférieur à 1/10 et les malvoyants dont l’acuité est au-dessus.</p>
+            <div class="main__article--top-left--partie-2">Elle se manifeste sous de nombreuses formes ; une vision floue, réduite en forte luminosité ou pénombre, un champ rétréci ou entrecoupé, une absence de relief, de couleur.
+            </div>  
+           </div>
+
+        </div>
+
+        <div class="main__article--center">
+            <div class="main__article--center-right"><p>En France, on recense 1 500 000 personnes malvoyantes et <br>70 000 aveugles. Les aveugles représentent seulement 14 % des déficients visuels.
+            </p></div>
+        </div>
+
+        <div class="main__article--bottom">
+            <div class="main__article--title"><h2>Obstacles numériques</h2></div>
+            <div class="main__article--bottom-left"><p>Les personnes en situation de handicap sont régulièrement confrontées à des barrières. On peut citer en guise d'exemples :
+</p></div>
+        </div>
+</div>
+<div class="main__footer">
+        <ul>
+            <a href="#" ><li>Suivant</li></a>
+            <a href="notions.php" ><li>Précédent</li></a>
+            <a href="#" ><li>Retour à la navigation</li></a>
+        </ul>
+    </div>
+</body>
+</html>
