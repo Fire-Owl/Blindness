2de4e10ea444185af409856145def23ff0c83508
acs <c.vauthier@codeur.online>
2021-06-01 16:28:24 +0200

    tabindex chloe

--- pages/bonnes-pratiques.php
+++ pages/bonnes-pratiques.php
@@ -2,57 +2,57 @@
 
     <div class="main">
         <div class="main__title">
-            <h1>IV. Bonnes pratiques</h1>
+            <h1 tabindex="0">IV. Bonnes pratiques</h1>
         </div>
 
         <div class="main__article--top">
             <div class="main__article--title">
-                <h2>La sobriété</h2>
+                <h2 tabindex="0">La sobriété</h2>
             </div>
         </div>
                 
         <div class="main__article--top-left">
-            <p>On réduit au strict minimum les effets dynamiques. On doit pouvoir visualiser <span>mentalement</span> les pages web. Dans le même ordre d'idée, on évite les ouvertures d’onglet externe qui pourrait perdre l'utilisateur.</p>
+            <p tabindex="0">On réduit au strict minimum les effets dynamiques. On doit pouvoir visualiser <span>mentalement</span> les pages web. Dans le même ordre d'idée, on évite les ouvertures d’onglet externe qui pourrait perdre l'utilisateur.</p>
         </div>
 
         <div class="main__article--top">
             <div class="main__article--title">
-                <h2>Un contenu structuré</h2>
+                <h2 tabindex="0">Un contenu structuré</h2>
             </div>
         </div>
                 
         <div class="main__article--top-left">
-            <p>Cela se traduit par <span>l'uniformisation</span> des mises en pages du site. La personne va gagner en aisance au fur et à mesure de la navigation. Toujours dans un souci de clarté, on s'assure de bien hiérarchiser les titres.</p>
+            <p tabindex="0">Cela se traduit par <span>l'uniformisation</span> des mises en pages du site. La personne va gagner en aisance au fur et à mesure de la navigation. Toujours dans un souci de clarté, on s'assure de bien hiérarchiser les titres.</p>
         </div>
 
         <div class="main__article--top">
             <div class="main__article--title">
-                <h2>Une syntaxe rigoureuse </h2>
+                <h2 tabindex="0">Une syntaxe rigoureuse </h2>
             </div>
         </div>
                 
         <div class="main__article--top-left">
-            <p>Chaque balise html a une <span>fonction précise</span>, qu'il est nécessaire de respecter pour assurer le bon fonctionnement des synthèses vocales.</p>
+            <p tabindex="0">Chaque balise html a une <span>fonction précise</span>, qu'il est nécessaire de respecter pour assurer le bon fonctionnement des synthèses vocales.</p>
         </div>
 
         <div class="main__article--top">
             <div class="main__article--title">
-                <h2>Des palliatifs textuels</h2>
+                <h2 tabindex="0">Des palliatifs textuels</h2>
             </div>
         </div>
                 
         <div class="main__article--top-left">
-            <p>Le label aria est un attribut qui permet aux développeurs d'indiquer une <span>alternative textuelle</span> à des contrôles visuels. De même pour l’attribut alt qui constitue une alternative aux images.</p>
+            <p tabindex="0">Le label aria est un attribut qui permet aux développeurs d'indiquer une <span>alternative textuelle</span> à des contrôles visuels. De même pour l’attribut alt qui constitue une alternative aux images.</p>
         </div>
 
         <div class="main__article--top">
             <div class="main__article--title">
-                <h2>Une police adaptée</h2>
+                <h2 tabindex="0">Une police adaptée</h2>
             </div>
         </div>
                 
         <div class="main__article--top-left">
-            <p>L’utilisation d’une police d'écriture <span>contrastée</span> offre une meilleure lecture et réduit la fatigue oculaire. La taille et l'épaisseur des caractères sont aussi à prendre en considération.</p>
+            <p tabindex="0">L’utilisation d’une police d'écriture <span>contrastée</span> offre une meilleure lecture et réduit la fatigue oculaire. La taille et l'épaisseur des caractères sont aussi à prendre en considération.</p>
         </div>
 
     </div>
--- pages/statistiques.php
+++ pages/statistiques.php
@@ -2,55 +2,55 @@
 
     <div class="main">
         <div class="main__title">
-            <h1>IV. Statistiques</h1>
+            <h1 tabindex="0">IV. Statistiques</h1>
         </div>
 
         <div id="charts">
             
-            <h2 class="charts__title">Navigateurs</h2>
+            <h2 class="charts__title" tabindex="0">Navigateurs</h2>
             <div class="charts__container">
                 <div class="charts__item">
-                    <canvas id="browser"></canvas>
+                    <canvas id="browser">Graphique des navigateurs les plus utilisés par les personnes aveugles pour naviguer sur internet.</canvas>
                 </div>
-                <p class="charts__item charts__item--paragraph">Les navigateurs les plus utilisés par les personnes aveugles pour naviguer sur internet sont Firefox, Internet Explorer et Safari.</p>
+                <p class="charts__item charts__item--paragraph" tabindex="0">Les navigateurs les plus utilisés par les personnes aveugles pour naviguer sur internet sont Firefox, Internet Explorer et Safari.</p>
             </div>
 
-            <h2 class="charts__title">Lecteurs d'écrans</h2>
+            <h2 class="charts__title" tabindex="0">Lecteurs d'écrans</h2>
             <div class="charts__container">
-            <p class="charts__item charts__item--paragraph charts__item--inverse">Les personnes aveugles utilisent majoirtairement un lecteur d'écran pour naviguer sur internet.</p>
+            <p class="charts__item charts__item--paragraph charts__item--inverse" tabindex="0">Les personnes aveugles utilisent majoirtairement un lecteur d'écran pour naviguer sur internet.</p>
                 <div class="charts__item">
-                    <canvas id="screen-reader"></canvas>
+                    <canvas id="screen-reader">Graphique représentant l'usage des lecteurs d'écrans par les personnes aveugles pour naviguer sur internet.</canvas>
                 </div>
                
             </div>
 
 
-            <h2 class="charts__title">Switch Tools</h2>
+            <h2 class="charts__title" tabindex="0">Lecteurs d'écrans différents </h2>
             <div class="charts__container">
                 <div class="charts__item">
-                    <canvas id="switch-tools"></canvas>
+                    <canvas id="switch-tools" >Graphique représentant les usages de différents lecteurs d'écrans au travail et à domicile.</canvas>
                 </div>
-                <p class="charts__item charts__item--paragraph">Les personnes aveugles utilisent majoirtairement un lecteur d'écran pour naviguer sur internet.
+                <p class="charts__item charts__item--paragraph" tabindex="0">Sur leur lieu de travail et à leur domicile, les personnes aveugles peuvent disposer de lecteurs d'écrans différents. Les statistiques montrent que les personnes aveugles utilisent plutôt majoirtairement le même lecteur d'écrans pour naviguer sur internet.
                 </p>
             </div>
 
-            <h2 class="charts__title">Types d'écran</h2>
+            <h2 class="charts__title" tabindex="0">Types d'écrans</h2>
             <div class="charts__container">
-                <p class="charts__item charts__item--paragraph charts__item--inverse">Les 3 types d'écrans utilisés par les personnes aveugles sont NVDA, JAWS et autres.
+                <p class="charts__item charts__item--paragraph charts__item--inverse" tabindex="0">Les 3 types d'écrans utilisés par les personnes aveugles sont NVDA, JAWS et autres.
                 </p>
                 <div class="charts__item">
-                    <canvas id="type-screen"></canvas>
+                    <canvas id="type-screen">Graphique représentant les types de lecteurs d'écrans préférés des utilisateurs aveugles.</canvas>
                 </div>
                
             </div>
 
 
-            <h2 class="charts__title">Smartphones</h2>
+            <h2 class="charts__title" tabindex="0">Smartphones</h2>
             <div class="charts__container">
                 <div class="charts__item">
-                    <canvas id="phone-tools"></canvas>
+                    <canvas id="phone-tools">Graphique des marques de téléphones mobiles utilisés pour naviguer sur internet.</canvas>
                 </div>
-                <p class="charts__item charts__item--paragraph">Les iphones Apple sont les appareils mobiles préférés utilisés par les aveugles en terme d'accessibilité visuelle pour naviguer sur internet. 
+                <p class="charts__item charts__item--paragraph" tabindex="0">Les iphones Apple sont les appareils mobiles préférés utilisés par les aveugles en terme d'accessibilité visuelle pour naviguer sur internet. 
                 </p>
             </div>
 
