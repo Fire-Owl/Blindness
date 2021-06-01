20e726117b2d6d4a85e67fc7fa0a8d1b96521fe5
Gaël Sudour-Jeannin <67095523+Sastaras@users.noreply.github.com>
2021-06-01 11:01:39 +0200

    Fixed updated website

--- includes/header.php
+++ includes/header.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <title>Blindness</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Un site sur l'accessibilité optimisé pour les malvoyants. Créé par les stagiaires de l'ACS.">
-    <link rel="stylesheet" type="text/css" href="styles/header.css">
-    <link rel="stylesheet" type="text/css" href="styles/index.css">
+    <meta name="description"
+        content="Un site sur l'accessibilité optimisé pour les malvoyants. Créé par les stagiaires de l'ACS.">
+    <link rel="stylesheet" type="text/css" href="../styles/header.css">
+    <link rel="stylesheet" type="text/css" href="../styles/index.css">
+    <link rel="stylesheet" type="text/css" href="../styles/notions.css">
 </head>
 
 <body>
     <div class="nav">
-        <a href="index.php"><img src="assets/logo/blindness.svg" alt="Logo du site Blindness" class="logo">
+        <a href="index.php"><img src="../assets/logo/blindness.svg" alt="Logo du site Blindness" class="logo">
             <h1 class="logo-text noselect">Blindness</h1>
         </a>
         <label for="toggle" class="noselect">Navigation</label>
         <input type="checkbox" id="toggle" />
         <div class="menu">
             <div class="nav-menu-romanum">
-                <a href="#">I.</a>
-                <a href="#">II.</a>
+                <a href="notions.php">I.</a>
+                <a href="deficiences-visuelles.php">II.</a>
                 <a href="#">III.</a>
                 <a href="#">IV.</a>
             </div>
             <div class="nav-menu-name">
-                <a href="#">NOTIONS</a>
-                <a href="#">DEFICIENCES VISUELLES</a>
+                <a href="notions.php">NOTIONS</a>
+                <a href="deficiences-visuelles.php">DEFICIENCES VISUELLES</a>
                 <a href="#">OUTILS</a>
                 <a href="#">BONNES PRATIQUES</a>
             </div>
