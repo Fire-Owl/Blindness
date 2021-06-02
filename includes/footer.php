84637840f46d15adc32666721259e2ebd36f0b56
Gaël Sudour-Jeannin <67095523+Sastaras@users.noreply.github.com>
2021-06-02 16:33:28 +0200

    cleaning

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-<a href="mentions.php" class="footer-link">Mentions légales</a>
-    <label for="toggle" class="noselect">Navigation</label>
-        <input type="checkbox" id="toggle" />
-</footer>
-
-</body>
-</html>
