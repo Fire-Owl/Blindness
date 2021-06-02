4b0e5d50976d4690a5ec6190924cce4aef4be7fb
Gaël Sudour-Jeannin <67095523+Sastaras@users.noreply.github.com>
2021-06-02 16:28:23 +0200

    merged

--- pages/notions.php
+++ pages/notions.php
@@ -1,23 +1,24 @@
-<?php include "../includes/header.php";?>
+<?php $current="notions";
+include "../includes/header.php";?>
 
-<div class="main">
+<div class="main bg1">
     <div class="main__title">
-        <h1>I. Notions</h1>
+        <h1 tabindex="0">I. Notions</h1>
     </div>
 
     <div class="main__article--top">
         <div class="main__article--title">
             <h2>Handicap</h2>
         </div>
-        <p>Le handicap est une notion complexe et évolutive. Il désigne l'incapacité d'un individu à vivre et agir
+        <p tabindex="0">Le handicap est une notion complexe et évolutive. Il désigne l'incapacité d'un individu à vivre et agir
             pleinement dans son environnement, en raison de déficiences physiques, mentales ou sensorielles.</p>
-        <p>À partir de 1980, le terme prend une dimension <span>sociale</span>. On prend conscience que ce qui créer la
+        <p tabindex="0">À partir de 1980, le terme prend une dimension <span>sociale</span>. On prend conscience que ce qui créer la
             situation de handicap résulte d'avantage d'un environnement inadapté que de la déficience elle-même.</p>
     </div>
 
     <div class="main__article--center">
         <div class="main__article--center-right">
-            <p>Le handicap souffre d’une image erronée, se résumant principalement à des personnes en fauteuils
+            <p tabindex="0">Le handicap souffre d’une image erronée, se résumant principalement à des personnes en fauteuils
                 roulants. Détrompez-vous, 80 à 85% des situations de handicap sont de nature <span>imperceptible</span>.
                 En effet, comment pourriez-vous devinez qu'untel souffre de diabète et qu'un autre souffre de dyslexie ?
             </p>
@@ -26,28 +27,13 @@
 
     <div class="main__article--bottom">
         <div class="main__article--title">
-            <h2>Accessibilité</h2>
+            <h2 tabindex="0">Accessibilité</h2>
         </div>
         <div class="main__article--bottom-left">
-            <p>Elle peut se traduire par l’accès à <span>tous</span> pour tous. Dans notre cadre, elle consiste à mettre
+            <p tabindex="0">Elle peut se traduire par l’accès à <span class="red">tous</span> pour tous. Dans notre cadre, elle consiste à mettre
                 les personnes en situation de handicap au même pied d'égalité que les autres. Pour cela on essaie de
                 réduire au minimum les inégalités liées à l'environnement. </p>
         </div>
     </div>
 </div>
-<div class="main__footer">
-    <ul>
-        <a href="deficiences-visuelles.php">
-            <li>Suivant</li>
-        </a>
-        <a href="#">
-            <li>Précédent</li>
-        </a>
-        <a href="#">
-            <li>Retour à la navigation</li>
-        </a>
-    </ul>
-</div>
-</body>
-
-</html>
+<?php include "../includes/footer.php";?>
